8cee9ed2c7e3a971a6b5cdc08a92d92b35a0a541
tkoljonen-wq <tkoljonen@gmail.com>
2026-04-05 09:53:41 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// Täytä nämä Firebase-konsolista (Project settings > Your apps > Firebase SDK snippet)
+export const firebaseConfig = {
+  apiKey: "AIzaSyAY6ehCGCcaZPv2VSHzrhQBg8-LtClVDlk",
+  authDomain: "rottien-pyydystys.firebaseapp.com",
+  projectId: "rottien-pyydystys",
+  storageBucket: "rottien-pyydystys.firebasestorage.app",
+  messagingSenderId: "442351384573",
+  appId: "1:442351384573:web:c412d88924d0f022280b94"
+};
